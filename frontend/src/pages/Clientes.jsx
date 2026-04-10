5c2528f4f62f25311fe56c60a427dc31d7a20c06
EPimentelContabilidade <ceampimentel@gmail.com>
2026-04-10 15:45:01 -0300

    Update Clientes.jsx

--- frontend/src/pages/Clientes.jsx
+++ frontend/src/pages/Clientes.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import { Search, Plus, X, Save, ChevronLeft, ChevronRight, User, MapPin, Phone, CheckCircle, Zap, Trash2, Eye, EyeOff, ExternalLink, Shield, FileText } from 'lucide-react'
 import GerarObrigacoes from './GerarObrigacoes'
 
